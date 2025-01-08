479f6170ee1da05dfdab5eac1081c58bd81342cd
estar-web <139598319+estar-web@users.noreply.github.com>
2025-01-08 22:58:39 +0900

    [update]_料金改定

--- _src/single-works.php
+++ _src/single-works.php
@@ -21,54 +21,18 @@
           <img src="<?php echo get_template_directory_uri(); ?>/assets/images/picture-dummy.webp" width="310" height="180" alt="非公開画像" loading="lazy" decoding="async">
         </figure><!-- /.p-achieve-icatch__img -->
       <?php else : ?>
-        <div class="swiper p-achieve-icatch__swiper">
-          <div class="swiper-wrapper p-achieve-icatch__swiperWrapper">
-            <div class="swiper-slide p-achieve-icatch__slide">
-              <?php
-              $pc_image = get_field('icatch_pc');   //pcイメージの情報を取得
-              $tab_image = get_field('icatch_tab'); //tabイメージの情報を取得
-              $sp_image = get_field('icatch_sp');   //spイメージの情報を取得
+        <?php
+        $pc_image = get_field('icatch_pc'); //pcイメージの情報を取得
 
-              // PC画像のurlとalt情報を取得
-              if (!empty($pc_image)) :
-                $url = $pc_image['url']; //画像のURLを取得
-                $alt = $pc_image['alt']; //画像のaltを取得
-                // $url = wp_get_attachment_url($pc_image);
-                // $alt = get_post_meta(get_post($pc_image) -> ID, '_wp_attachment_image_alt' , true );
-              ?>
-                <figure class="p-achieve-icatch__img p-icatch--pc">
-                  <img src="<?php echo esc_url($url); ?>" width="310" height="180" alt="<?php echo esc_attr($alt); ?>" loading="lazy" decoding="async">
-                </figure><!-- /.p-achieve-icatch__img -->
-              <?php endif; ?>
-            </div>
-            <div class="swiper-slide p-achieve-icatch__slide">
-              <?php
-              // PC画像のurlとalt情報を取得
-              if (!empty($tab_image)) :
-                $url = $tab_image['url']; //画像のURLを取得
-                $alt = $tab_image['alt']; //画像のaltを取得 
-              ?>
-                <figure class="p-achieve-icatch__img p-icatch--tab">
-                  <img src="<?php echo $url; ?>" width="166" height="180" alt="<?php echo $alt; ?>" loading="lazy" decoding="async">
-                </figure><!-- /.p-achieve-icatch__img -->
-              <?php endif; ?>
-            </div>
-            <div class="swiper-slide p-achieve-icatch__slide">
-              <?php
-              // PC画像のurlとalt情報を取得
-              if (!empty($sp_image)) :
-                $url = $sp_image['url']; //画像のURLを取得
-                $alt = $sp_image['alt']; //画像のaltを取得
-              ?>
-                <figure class="p-achieve-icatch__img p-icatch--sp">
-                  <img src="<?php echo $url; ?>" width="83" height="180" alt="<?php echo $alt; ?>" loading="lazy" decoding="async">
-                </figure><!-- /.p-achieve-icatch__img -->
-              <?php endif; ?>
-            </div>
-          </div>
-          <!-- ページネーション -->
-        </div>
-        <div class="swiper-pagination p-achieve-icatch__pagination"></div>
+        // PC画像のurlとalt情報を取得
+        if (!empty($pc_image)) :
+          $url = $pc_image['url']; //画像のURLを取得
+          $alt = $pc_image['alt']; //画像のaltを取得
+        ?>
+          <figure class="p-achieve-icatch__img p-icatch--pc">
+            <img src="<?php echo esc_url($url); ?>" width="310" height="180" alt="<?php echo esc_attr($alt); ?>" loading="lazy" decoding="async">
+          </figure><!-- /.p-achieve-icatch__img -->
+        <?php endif; ?>
       <?php endif ?>
     </div><!-- /.p-achieve-icatch -->
 
